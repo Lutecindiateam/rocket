f000cec5af41c4ed41634225ca0b4a8b54e9a1f4
Mahesh Raut <maheshraut0404@gmail.com>
2023-08-12 11:59:28 +0530

    13th commit

--- client/src/sendbirdConnection.js
+++ client/src/sendbirdConnection.js
@@ -55,16 +55,16 @@ function App(props){
         }
       }, [props.employee.empdata]);
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyD8Mkt7IEJPF4jf5S974xgftQph4TWaD_I",
-        authDomain: "job-portal-348803.firebaseapp.com",
-        databaseURL: "https://job-portal-348803-default-rtdb.firebaseio.com/",
-        projectId: "job-portal-348803",
-        storageBucket: "job-portal-348803.appspot.com",
-        messagingSenderId: "31060957964",
-        appId: "1:31060957964:web:c3f62d163b5bdebc488729",
-        measurementId: "G-8M788WG3HQ"
-      };
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyD8Mkt7IEJPF4jf5S974xgftQph4TWaD_I",
+    //     authDomain: "job-portal-348803.firebaseapp.com",
+    //     databaseURL: "https://job-portal-348803-default-rtdb.firebaseio.com/",
+    //     projectId: "job-portal-348803",
+    //     storageBucket: "job-portal-348803.appspot.com",
+    //     messagingSenderId: "31060957964",
+    //     appId: "1:31060957964:web:c3f62d163b5bdebc488729",
+    //     measurementId: "G-8M788WG3HQ"
+    //   };
       
       // Initialize Firebase
       const app = initializeApp(firebaseConfig);
